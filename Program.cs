c994e4ea1ce96e7c79c5e39aac48ad5eb280a352
Grimace of Despair <GrimaceOfDespair@hotmail.com>
2012-10-03 16:49:33 +0200

    Add code

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UrlChecker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string line;
+      while (string.IsNullOrEmpty(line = Console.ReadLine()) == false)
+      {
+        try
+        {
+          //Console.WriteLine("Checking " + line);
+          CheckUrl(Regex.Replace(line, @"\t(.*)$", ""));
+        }
+        catch (Exception e)
+        {
+          Console.Error.WriteLine(e);
+        }
+      }
+    }
+
+    private static void CheckUrl(string url)
+    {
+      var request = (HttpWebRequest)WebRequest.Create(url);
+      request.AllowAutoRedirect = true;
+      request.MaximumAutomaticRedirections = 4;
+
+      HttpStatusCode statusCode;
+      Uri responseUri;
+      using (var response = (HttpWebResponse)request.GetResponse(false))
+      {
+        statusCode = response.StatusCode;
+        responseUri = response.ResponseUri;
+      }
+
+      if (statusCode == HttpStatusCode.OK)
+      {
+        Console.WriteLine(url + "\t" + statusCode + "\t" + responseUri);
+      }
+      else
+      {
+        Console.Error.WriteLine(url + "\t" + statusCode + "\t" + responseUri);
+      }
+    }
+  }
+}
---
+++ WebRequestExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace UrlChecker
+{
+  public static class WebRequestExtensions
+  {
+    public static WebResponse BetterEndGetResponse(this WebRequest request, IAsyncResult asyncResult)
+    {
+      try
+      {
+        return request.EndGetResponse(asyncResult);
+      }
+      catch (WebException wex)
+      {
+        if (wex.Response != null)
+        {
+          return wex.Response;
+        }
+        throw;
+      }
+    }
+
+    public static WebResponse GetResponse(this WebRequest request, bool doThrow)
+    {
+      try
+      {
+        return request.GetResponse();
+      }
+      catch (WebException wex)
+      {
+        if (doThrow == false && wex.Response != null)
+        {
+          return wex.Response;
+        }
+        throw;
+      }
+    }
+  }
+}
